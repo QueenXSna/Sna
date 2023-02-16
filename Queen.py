e3b08d0bccf4964ef1136cd4ff86ea560b4467fd
QueenXSna <125026134+QueenXSna@users.noreply.github.com>
2023-02-17 01:00:53 +0500

    Create Queen.py

---
+++ Queen.py
@@ -0,0 +1,17 @@
+import os,platform
+
+os.system('git pull')
+
+ import os
+
+os.system('termux-setup-storage')
+
+os.system('xdg-open https://facebook.com/groups/554452446624067/ ')
+
+import SNA 
+
+menu()
+ 
+
+ 
+
